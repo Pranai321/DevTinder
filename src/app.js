37a6cd0fb5035d63850aff0d17f0959ce52a90e6
kalva pranai sai reddy <kalvapranaisaireddy@gmail.com>
2025-06-15 19:37:39 -0700

    error handling

--- src/app.js
+++ src/app.js
@@ -4,33 +4,25 @@ const express = require("express");
 
 const app = express();
 
-app.use('/user',Auth);
-
-app.get("/user",(req,res)=>{
-    res.send({name:"pranai",age: 25});
-})
-
-// we can send the Auth middleware as 1st request handler, if Auth is success, it calls the next request handle
-// app.get('/user', Auth, (req,res)=>{
-//     res.send("Authenticated successfully");
+// app.get("/getuserdata",(req,res)=>{
+//     try{
+//         //logic
+//         throw new Error("Object");
+//     }
+//     catch(err){
+//         res.send(err.message);
+//     }
 // })
+app.get('/getuserdata',(req,res)=>{
 
-app.post("/user",(req,res)=>{
-    res.send("data saved in the DB successfully");
+    throw new Error("error object");
+});
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        console.log(err.message);   
+        res.send("something went wrong")
+    }
 })
-
-app.put("/user",(req,res)=>{
-    res.send("updated the profile");
-})
-
-app.patch("/user", (req,res)=>{
-    res.send("patched the profile");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("deleted the data")
-})
-
 app.listen(1777, ()=>{
     console.log("server is up and running on portal 1777");
 })
