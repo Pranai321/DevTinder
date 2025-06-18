04eb09b03e5b0fc3cf77ddd8fc47cd55816d3cb9
kalva pranai sai reddy <kalvapranaisaireddy@gmail.com>
2025-06-17 23:09:48 -0700

    created userSchema, userModel, added to userdetails into the database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const {Auth} = require('./middlewares/auth.js');
 const express = require("express");
-
 const connectDB = require('./config/database.js');
+const User = require('./models/user.js');
 
 const app = express(); 
 
+app.post('/signup',async (req,res)=>{
+    const user = new User({
+        firstName:"pranai",
+        lastName:"Reddy",
+        emailId:"pranaisaireddy@gmail.com"
+    })
+    await user.save()
+        .then(()=>{
+        res.send("User signed-up")
+    })
+        .catch((err)=>{
+            res.send("error saving user details in the DB")
+        })
+})
 connectDB().then(()=>{
     console.log("App is connected to DB");
     app.listen(1777, ()=>{
